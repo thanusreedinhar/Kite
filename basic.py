8ad4c93059e5447631d1cdd72f4b4f4590dcc1d0
thanusreedinhar <thanusreedinhar@gmail.com>
2025-11-22 13:41:02 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,29 @@
+a=[10,20,30,40,50,50,50,50]
+a.append(60)
+print(a)
+a.insert(1,15)
+print(a)
+print(15 in a)
+print(a.count(50))
+a.remove(20)
+print(a)
+a.pop(3)
+print(a)
+del a[1]
+max(a)
+print (max(a))
+min(a)
+print(min(a))
+print(sum(a))
+a.reverse()
+print(a)
+print(a.sort())
+a.sort()
+print(a)
+print(sorted(a))
+c=15.5
+t=7
+s=c//t
+print(s)
+print(5+4-2)
+print(2**2**-1)
---
+++ logical.py
@@ -0,0 +1,38 @@
+a=20
+b=30
+c=50
+#print(a<b and b<c)
+#print(a<b or b>c)
+#print(not a<b)
+#tuple
+t=(20,20,30)
+print(t[0])
+#t[1]=30
+#print(t)
+
+#dictionary
+alpha={"a":1,"b":2,"c":3}
+#print(alpha[a])
+alpha['a']=5
+print(alpha)
+#print(["b"]=5)
+
+#set
+s=set((10,20,30,30,40,40))
+print(s)
+a=1
+print(not a)
+x=32
+print(x<<2)
+print(x<<1)
+print(x>>2)
+print(x>>1)
+print(x>>3)
+
+x=5
+print(x<<2)
+print(x<<1)
+print(x>>2)
+print(x>>1)
+print(x>>3)
+
